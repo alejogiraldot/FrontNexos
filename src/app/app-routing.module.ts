839794e8304b4981d75a31d7e689acabac53bee8
Tartaruga <alej409@hotmail.com>
2023-09-05 22:18:06 -0500

    Perdón nexos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'merchandise',
+    loadChildren: () =>
+      import('./merchandise/merchandise.module').then(
+        (m) => m.MerchandiseModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
